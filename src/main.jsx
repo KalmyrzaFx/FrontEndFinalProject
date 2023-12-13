65213681c5aad04cad45729d5b6746db100295a1
Kalmyrza <zholchubekovk@gmail.com>
2023-12-13 13:09:03 +0600

    deleted old version of app

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-
-const root = document.getElementById('root');
-
-// Use createRoot to render your application
-const appRoot = createRoot(root);
-appRoot.render(<App />);
